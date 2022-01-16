e4e55b576223883689535d357142d6c1e8f7e0e8
Janmejay16 <janmejay.shastri01@gmail.com>
2022-01-16 11:36:31 +0530

    added components

---
+++ division.js
@@ -0,0 +1,24 @@
+const div = {
+    Home: [
+        "About org",
+        "FAQs",
+        "Contact Us (Minimal Form)"
+    ],
+    Event: [
+        "About Event (detail)",
+        "Tracks",
+        "Schedule",
+        "Prizes",
+        "Winners",
+        "Sponsors"
+    ],
+    Team: [
+        "Mentors",
+        "Team",
+        "Developers"
+    ],
+    Contact: [
+        "Links, email + Form",
+        "Subscribe to newsletter/notifications"
+    ]
+}
